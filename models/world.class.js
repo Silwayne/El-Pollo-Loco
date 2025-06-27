c20ced3796833802c3fdbbbb0eb737d1a5821268
Ronny Pollmer <168807956+Silwayne@users.noreply.github.com>
2025-06-27 10:41:35 +0200

    Clouds and Math added

--- models/world.class.js
+++ models/world.class.js
@@ -1,6 +1,13 @@
 class World {
   character = new Character();
-  enemies = [new Chicken(), new Chicken(), new Chicken()];
+  enemies = [
+    new Chicken(),
+    new Chicken(), 
+    new Chicken()
+  ];
+  clouds = [
+    new Cloud(),
+    ];
   canvas;
   ctx;
 
@@ -11,14 +18,20 @@ class World {
   }
 
   draw() {
+    // Leere den Canvas
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
     this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
+    // Zeichne alle Feinde
     this.enemies.forEach(enemy => {
       this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.width, enemy.height);
     });
 
-    // Draw() wird immer wieder ausgeführt
+    this.clouds.forEach(cloud => {
+      this.ctx.drawImage(cloud.img, cloud.x, cloud.y, cloud.width, cloud.height);
+    });
+
+    // draw() wird immer wieder ausgeführt
     let self = this;
     requestAnimationFrame(function() {
       self.draw();

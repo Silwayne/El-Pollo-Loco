76ae3fca1b7c56755e72774fe36c27c8a209ba88
Ronny Pollmer <168807956+Silwayne@users.noreply.github.com>
2025-07-01 12:09:18 +0200

    Background, architektur

--- models/world.class.js
+++ models/world.class.js
@@ -1,18 +1,15 @@
 class World {
   character = new Character();
-  enemies = [
-    new Chicken(),
-    new Chicken(), 
-    new Chicken()
+  enemies = [new Chicken(), new Chicken(), new Chicken()];
+  clouds = [new Cloud()];
+  backgroundObjects = [
+    new BackgroundObject("img/5_background/layers/3_third_layer/1.png", 0, 100),
   ];
-  clouds = [
-    new Cloud(),
-    ];
   canvas;
   ctx;
 
   constructor(canvas) {
-    this.ctx = canvas.getContext('2d');
+    this.ctx = canvas.getContext("2d");
     this.canvas = canvas;
     this.draw();
   }
@@ -21,20 +18,24 @@ class World {
     // Leere den Canvas
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-    this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
-    // Zeichne alle Feinde
-    this.enemies.forEach(enemy => {
-      this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.width, enemy.height);
-    });
-
-    this.clouds.forEach(cloud => {
-      this.ctx.drawImage(cloud.img, cloud.x, cloud.y, cloud.width, cloud.height);
-    });
-
+    this.addToMap(this.character);
+    this.addObjectToMap(this.enemies);
+    this.addObjectToMap(this.clouds);
+    this.addObjectToMap(this.backgroundObjects);
     // draw() wird immer wieder ausgeführt
     let self = this;
-    requestAnimationFrame(function() {
+    requestAnimationFrame(function () {
       self.draw();
     });
+  }
+
+  addObjectToMap(objects) {
+    objects.forEach(o => {
+      this.addToMap(o);
+    });
+  }
+
+  addToMap(mo) {
+    this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
   }
 }

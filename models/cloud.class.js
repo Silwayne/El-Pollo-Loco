15883161c4b4ecf04034db31dfaf88b8273bd6c2
Ronny Pollmer <168807956+Silwayne@users.noreply.github.com>
2025-07-14 17:06:13 +0200

    Clouds animated, pepe walk images added

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -7,5 +7,12 @@ class Cloud extends MovableObject {
     super().loadImage("img/5_background/layers/4_clouds/1.png");
 
     this.x = Math.random() * 500; // Zufällige Position auf der X-Achse
+    this.animate();
+  }
+
+  animate() {
+    setInterval(() => {
+      this.x -= 0.15; // Bewegt die Wolke nach links
+    }, 1000 / 60); // 60 FPS
   }
 }

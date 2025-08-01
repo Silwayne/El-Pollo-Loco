83d25d28bee1a01b3b73801cb77094b7bdec28d9
Ronny Pollmer <168807956+Silwayne@users.noreply.github.com>
2025-08-01 14:49:42 +0200

    Keyboard

--- models/character.class.js
+++ models/character.class.js
@@ -11,6 +11,8 @@ class Character extends MovableObject {
     "img/2_character_pepe/2_walk/W-26.png",
   ];
 
+  world;
+
   constructor() {
     super().loadImage("img/2_character_pepe/2_walk/W-21.png");
     this.loadImages(this.IMAGES_WALKING);
@@ -20,11 +22,13 @@ class Character extends MovableObject {
 
   animate() {
     setInterval(() => {
-      let i = this.currentImage % this.IMAGES_WALKING.length; // Modulus-Operator, um den Index zu begrenzen
-      let path = this.IMAGES_WALKING[i]; // Mit dem i startet das Array wieder von vorne
-      this.img = this.imageCache[path]; // Greift auf das Bild im Cache zu
-      this.currentImage++; // Erhöht den Index für das nächste Bild
-    }, 200); 
+      if (this.world.keyboard.RIGHT) {
+        let i = this.currentImage % this.IMAGES_WALKING.length; // Modulus-Operator, um den Index zu begrenzen
+        let path = this.IMAGES_WALKING[i]; // Mit dem i startet das Array wieder von vorne
+        this.img = this.imageCache[path]; // Greift auf das Bild im Cache zu
+        this.currentImage++; // Erhöht den Index für das nächste Bild
+      }
+    }, 200);
   }
 
   jump() {}
